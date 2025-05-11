559b0de6c157ca13516648787d62eafbbd94d320
damla-nur00 <damlanurdalcali@gmail.com>
2025-05-11 14:42:39 +0300

    Update cartitem.dart

--- lib/app/cartitem.dart
+++ lib/app/cartitem.dart
@@ -1,17 +1,15 @@
-// cartitem.dart
 class CartItem {
   final String name;
   final String price;
   final String imagePath;
-  final String size; // Boyut ekleyelim
+  final String size; 
   int count = 1;
 
   CartItem({
     required this.name,
     required this.price,
     required this.imagePath,
-    required this.size, // Boyut burada da gerekli
+    required this.size,
     this.count = 1,
   });
 }
-
